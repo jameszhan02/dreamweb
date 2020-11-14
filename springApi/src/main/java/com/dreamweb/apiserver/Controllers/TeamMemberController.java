874d0ee511d68d63efeb5379e653bafda1392a26
Sheng Zhan <jameszhan02@hotmail.com>
2020-11-14 14:18:10 -0500

    swagger add

--- springApi/src/main/java/com/dreamweb/apiserver/Controllers/TeamMemberController.java
+++ springApi/src/main/java/com/dreamweb/apiserver/Controllers/TeamMemberController.java
@@ -4,6 +4,7 @@ package com.dreamweb.apiserver.Controllers;
 import com.dreamweb.apiserver.Entities.TeamMemberEntity;
 import com.dreamweb.apiserver.Repositories.TeamMemberRepository;
 import com.dreamweb.apiserver.Services.TeamMemberService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @CrossOrigin
 @RequestMapping(path="/teamMember")
+@Api(value = "testController|一个用来测试swagger注解的控制器")
 public class TeamMemberController  {
 //
 //    @Autowired
