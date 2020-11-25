bc26ed3bbb56455496b7f60f8f019c2d31cf0e16
Sheng Zhan <jameszhan02@hotmail.com>
2020-11-25 14:31:36 -0500

    format added

--- src/modules/UserData.js
+++ src/modules/UserData.js
@@ -37,7 +37,6 @@ const useStyles = makeStyles((theme) => ({
     form: {
       width: "100%", // Fix IE 11 issue.
       marginTop: theme.spacing(1),
-    
     },
     submit: {
       margin: theme.spacing(2, 0, 0),
