3f81b9caa4dc5adb79338415ce16028c7b6a3263
詹晟 <sillyknight@zhanshengdeMBP.hitronhub.home>
2020-11-29 17:00:23 -0500

    uptodate push

--- springApi/src/main/java/com/dreamweb/apiserver/Controllers/UserController.java
+++ springApi/src/main/java/com/dreamweb/apiserver/Controllers/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 
 @Controller
@@ -36,13 +35,9 @@ public class UserController  {
     @PostMapping(path = "/signin")
     @ResponseBody
     public User SignIn(@RequestBody User user){
-        Boolean res = false;
         String email = user.getEmail();
         String password = user.getPassword();
         User returnUser = userRepository.findTopByEmailAndPassword(email,password);
-        
         return returnUser;
     }
-
-
 }
