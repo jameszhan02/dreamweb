785a419467c56ac5e4ba94fd65506e10c0c02cc0
CaseStudy1Complete <Stephan@SS-TP>
2020-12-03 14:49:47 -0500

    material ui updates

--- src/modules/NewPost.js
+++ src/modules/NewPost.js
@@ -1,61 +1,109 @@
 // import * as React from 'react'
-import React, {  useState } from 'react';
+import React, { useState } from "react";
 // import * as ReactDOM from 'react-dom'
-import MarkdownIt from 'markdown-it'
-import MdEditor from 'react-markdown-editor-lite'
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
 // import style manually
-import 'react-markdown-editor-lite/lib/index.css';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import "react-markdown-editor-lite/lib/index.css";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 import jwtAxios from "../axios";
-import {useHistory} from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 //MUST START WITH capital letter  (link below)
 //https://dev.to/ranewallin/js-bites-react-hook-is-called-in-a-function-which-is-neither-a-react-function-or-sic-a-custom-react-hook-1g2c
 const NewPost = () => {
-    const history = useHistory();
-    const [content, setContent] = useState([]);
-    const [title, setTitle] = useState([]);
-    const [desc, setDesc] = useState([]);
-    const mdParser = new MarkdownIt(/* Markdown-it options */);
-    function handleEditorChange({ html, text }) {
-        // console.log('handleEditorChange', html, text)
-        setContent(html);
-    }
-    const handleTitleChange = (e) => {
-        // console.log(e.target.value);
-        setTitle(e.target.value);
-    }    
-    const handleDescChange = (e) => {
-        // console.log(e.target.value);
-        setDesc(e.target.value);
-    }
-    const submitText = async() => {
-        let post = {postTitle: title, postText: desc, postUser: 1,postDate: Date.now(), postContent: content, postImgalt: `${title}img`, postImg:"" }
-        // // const res = await Axios.post('http://localhost:8080/user/signup', user);
-        let res = await jwtAxios.post(`/post/savepost`,post);
-        history.push('/postlist');
-        return res;
-    }
+  const history = useHistory();
+  const [content, setContent] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [desc, setDesc] = useState([]);
+  const mdParser = new MarkdownIt(/* Markdown-it options */);
+  function handleEditorChange({ html, text }) {
+    // console.log('handleEditorChange', html, text)
+    setContent(html);
+  }
+  const handleTitleChange = (e) => {
+    // console.log(e.target.value);
+    setTitle(e.target.value);
+  };
+  const handleDescChange = (e) => {
+    // console.log(e.target.value);
+    setDesc(e.target.value);
+  };
+  const submitText = async () => {
+    let post = {
+      postTitle: title,
+      postText: desc,
+      postUser: 1,
+      postDate: Date.now(),
+      postContent: content,
+      postImgalt: `${title}img`,
+      postImg: "",
+    };
+    // // const res = await Axios.post('http://localhost:8080/user/signup', user);
+    let res = await jwtAxios.post(`/post/savepost`, post);
+    history.push("/postlist");
+    return res;
+  };
 
-    return (
-        <div className="newpostContainer">
-            <TextField id="standard-basic" label="Title go here" onChange={handleTitleChange} />
-            <br/>
-            <br/>
-            <TextField id="standard-basic" label="Describe your Topic" onChange={handleDescChange} />
-            <br/>
-            <br/>
-            <MdEditor
-                style={{ height: "500px" }}
-                renderHTML={(text) => mdParser.render(text)}
-                onChange={handleEditorChange}
-            />
-            <br/>
-            <br/>
-            <Button variant="contained" color="primary" onClick={submitText}>Publish !</Button>
-        </div>
-    );
+  return (
+    <div
+      className="newpostContainer"
+      style={{ width: "50%", margin: "auto", padding: 10 }}
+    >
+      <Card>
+        <CardContent style={{ padding: 10 }}>
+          <Typography
+            variant="h6"
+            align="left"
+            color="primary"
+            style={{ padding: 5 }}
+          >
+            New Post
+          </Typography>
+          <TextField
+            id="standard-basic"
+            label="Title"
+            variant="outlined"
+            color="primary"
+            fullWidth="true"
+            size="small"
+            style={{ paddingBottom: 5 }}
+            onChange={handleTitleChange}
+          />
+          <TextField
+            id="standard-basic"
+            label="Description"
+            variant="outlined"
+            color="primary"
+            fullWidth="true"
+            size="small"
+            style={{ paddingBottom: 5 }}
+            onChange={handleDescChange}
+          />
+          <MdEditor
+            style={{ minHeight: 250, padding: 10 }}
+            renderHTML={(text) => mdParser.render(text)}
+            onChange={handleEditorChange}
+          />
+        </CardContent>
+        <CardActions>
+          <Button
+            style={{ marginLeft: "auto" }}
+            variant="contained"
+            color="primary"
+            onClick={submitText}
+          >
+            Publish
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
 };
 
 export default NewPost;
--- src/modules/PostList.js
+++ src/modules/PostList.js
@@ -1,48 +1,70 @@
-import React, { useEffect, useState  } from "react";
-import Axios from 'axios';
-import {listPost} from '../apiServices/dbTesterApi';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { listPost } from "../apiServices/dbTesterApi";
 import "./Post.css";
-import Button from '@material-ui/core/Button';
-import {useHistory} from 'react-router-dom';
+import TextField from "@material-ui/core/TextField";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import { useHistory } from "react-router-dom";
 
 const Post = () => {
   //get data
   const history = useHistory();
   const [postlist, setPostlist] = useState([]);
-    useEffect(() => {
-       Axios.all([
-        listPost(),
-      ]).then(([postlist]) => {
-        setPostlist(postlist);
-      });
-    }, []);
+  useEffect(() => {
+    Axios.all([listPost()]).then(([postlist]) => {
+      setPostlist(postlist);
+    });
+  }, []);
 
-    console.log(postlist);
+  console.log(postlist);
 
   const newPost = () => {
-    history.push('/newpost');
+    history.push("/newpost");
     return history;
-  }
-      return (
-        <div className="post-container">
-          <Button variant="contained" color="primary" onClick={newPost}>Add My Post</Button>
-          <br/>
-          <br/>
-          {postlist.map((item, index) => {
-            console.log(item.postImg);
-            return (
-              <div className="post-row" key={index}>
-                <div className="post-user">{item.postUser}</div>
-                <div className="post-date">{item.postDate}</div>
-                <div className="post-title">{item.postTitle}</div>
-                <div className="post-content">{item.postText}</div>
-                <img  src={item.postImg} alt={item.postImgalt}/>
-              </div>
-            );
-          })}
-        </div>
-      );
-  
-}
+  };
+  return (
+    <div className="post-container">
+      <Card style={{ width: "70%" }}>
+        <TextField
+          id="outlined-basic"
+          label="Create a new post"
+          variant="outlined"
+          color="primary"
+          fullWidth="true"
+          size="small"
+          InputLabelProps={{ shrink: false }}
+          onClick={newPost}
+        />
+      </Card>
+      <br />
+      <br />
+      {postlist.map((item, index) => {
+        console.log(item.postImg);
+        return (
+          <div key={index}>
+            <Typography
+              variant="caption"
+              display="block"
+              align="left"
+              gutterBottom
+            >
+              {item.postUser} {item.postDate}
+            </Typography>
+            <Typography variant="h6" align="left" color="primary">
+              {item.postTitle}
+            </Typography>
+            <div dangerouslySetInnerHTML={{ __html: item.postText }} />
+            <img
+              src={item.postImg}
+              alt={item.postImgalt}
+              style={{ width: "100%" }}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Post;
