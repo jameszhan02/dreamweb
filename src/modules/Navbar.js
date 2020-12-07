d78881b57d16fc47b5685c953ae4de724a290fa1
Sheng Zhan <jameszhan02@hotmail.com>
2020-12-07 12:48:59 -0500

    user icon and logout

--- src/modules/Navbar.js
+++ src/modules/Navbar.js
@@ -1,5 +1,17 @@
 import React, { Component } from "react";
 import "./Navbar.css";
+import {logout, loginUser } from '../cookieHelper';
+
+let currentUser = loginUser();
+let isUserLogin = false;
+if(currentUser){
+  console.log(currentUser);
+  isUserLogin = true;
+}
+
+const curUserLogout = () =>{
+  logout();
+}
 
 const MenuItems = [
   {
@@ -44,7 +56,12 @@ class Navbar extends Component {
             );
           })}
         </ul>
-        
+        <div className="iconBorder">
+        {isUserLogin
+        ? <img className="personIcon" src={currentUser.data.piclink} alt="piclink" onClick={curUserLogout}/>
+        : <img className="personIcon" src="https://user-images.githubusercontent.com/45243472/101384466-7be14280-3888-11eb-9e05-4efe863e4072.jpg" alt="defaultPic"/>
+        }
+        </div>
       </nav>
     );
   }
