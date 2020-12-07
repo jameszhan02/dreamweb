f8987dbf9857c48de38ae11e9fd657773adbb230
MartinMatianyi <martin.matianyi@gmail.com>
2020-12-07 01:27:10 -0500

    aboutUs

--- src/modules/AboutUs.js
+++ src/modules/AboutUs.js
@@ -28,7 +28,11 @@ const AboutUs = () => {
     return (
         <div>
             <LunBoComponent lunboObject={data.lunboObject} imgArray={data.imgArray} linkArray={data.linkArray}/>
-            <Row gutter={16}>
+           
+
+            <br />
+            <div className={styles.aboutusmid}>
+            <Row gutter={16} >
                 <Col span={12}>
                     <Statistic title="Feedback" value={1128} prefix={<LikeOutlined />} />
                 </Col>
@@ -36,114 +40,34 @@ const AboutUs = () => {
                     <Statistic title="Active Users" value={2308} />
                 </Col>
             </Row>
-
-            <br />
-
+            </div>
             <div className={styles.card}>
-            <div className={styles.container}></div>
-                I dont know what is wrong about this Man ) LMAO
-                And this is About Us for now
-                To add some length to it, let us mention that this page was
-primarily written for testing the effect of <strong>user style sheets</strong>.
-You can use it for various other purposes as well, like just checking how
-your browser displays various HTML elements by default.
-It can also be useful when testing conversions from HTML
-format to other formats, since some elements can go wrong then.
-This is another paragraph. I think it needs to be added that
-the set of elements tested is not exhaustive in any sense. I have selected
-those elements for which it can make sense to write user style sheet rules,
-To add some length to it, let us mention that this page was
-primarily written for testing the effect of <strong>user style sheets</strong>.
-You can use it for various other purposes as well, like just checking how
-your browser displays various HTML elements by default.
-It can also be useful when testing conversions from HTML
-format to other formats, since some elements can go wrong then.
-This is another paragraph. I think it needs to be added that
-the set of elements tested is not exhaustive in any sense. I have selected
-those elements for which it can make sense to write user style sheet rules,
-To add some length to it, let us mention that this page was
-primarily written for testing the effect of <strong>user style sheets</strong>.
-You can use it for various other purposes as well, like just checking how
-your browser displays various HTML elements by default.
-It can also be useful when testing conversions from HTML
-format to other formats, since some elements can go wrong then.
-This is another paragraph. I think it needs to be added that
-the set of elements tested is not exhaustive in any sense. I have selected
-those elements for which it can make sense to write user style sheet rules,
-To add some length to it, let us mention that this page was
-primarily written for testing the effect of <strong>user style sheets</strong>.
-You can use it for various other purposes as well, like just checking how
-your browser displays various HTML elements by default.
-It can also be useful when testing conversions from HTML
-format to other formats, since some elements can go wrong then.
-This is another paragraph. I think it needs to be added that
-the set of elements tested is not exhaustive in any sense. I have selected
-To add some length to it, let us mention that this page was
-primarily written for testing the effect of <strong>user style sheets</strong>.
-You can use it for various other purposes as well, like just checking how
-your browser displays various HTML elements by default.
-It can also be useful when testing conversions from HTML
-format to other formats, since some elements can go wrong then.
-This is another paragraph. I think it needs to be added that
-the set of elements tested is not exhaustive in any sense. I have selected
-those elements for which it can make sense to write user style sheet rules,
-To add some length to it, let us mention that this page was
-primarily written for testing the effect of <strong>user style sheets</strong>.
-You can use it for various other purposes as well, like just checking how
-your browser displays various HTML elements by default.
-It can also be useful when testing conversions from HTML
-format to other formats, since some elements can go wrong then.
-This is another paragraph. I think it needs to be added that
-the set of elements tested is not exhaustive in any sense. I have selected
-those elements for which it can make sense to write user style sheet rules,
-To add some length to it, let us mention that this page was
-primarily written for testing the effect of <strong>user style sheets</strong>.
-You can use it for various other purposes as well, like just checking how
-your browser displays various HTML elements by default.
-It can also be useful when testing conversions from HTML
-format to other formats, since some elements can go wrong then.
-This is another paragraph. I think it needs to be added that
-the set of elements tested is not exhaustive in any sense. I have selected
-those elements for which it can make sense to write user style sheet rules,
-To add some length to it, let us mention that this page was
-primarily written for testing the effect of <strong>user style sheets</strong>.
-You can use it for various other purposes as well, like just checking how
-your browser displays various HTML elements by default.
-It can also be useful when testing conversions from HTML
-format to other formats, since some elements can go wrong then.
-This is another paragraph. I think it needs to be added that
-the set of elements tested is not exhaustive in any sense. I have selected
-those elements for which it can make sense to write user style sheet rules,
-To add some length to it, let us mention that this page was
-primarily written for testing the effect of <strong>user style sheets</strong>.
-You can use it for various other purposes as well, like just checking how
-your browser displays various HTML elements by default.
-It can also be useful when testing conversions from HTML
-format to other formats, since some elements can go wrong then.
-This is another paragraph. I think it needs to be added that
-the set of elements tested is not exhaustive in any sense. I have selected
-those elements for which it can make sense to write user style sheet rules,
-To add some length to it, let us mention that this page was
-primarily written for testing the effect of <strong>user style sheets</strong>.
-You can use it for various other purposes as well, like just checking how
-your browser displays various HTML elements by default.
-It can also be useful when testing conversions from HTML
-format to other formats, since some elements can go wrong then.
-This is another paragraph. I think it needs to be added that
-the set of elements tested is not exhaustive in any sense. I have selected
-those elements for which it can make sense to write user style sheet rules,
-those elements for which it can make sense to write user style sheet rules,
+                
+            <div className={styles.aboutUsContainer}></div>
+            <h2>Chinese Gang</h2>
+            <p>
+            Chinese gang is a study group composed of 4 level 6 students from Fanshawe College, namely: </p>
+            <p><strong>Ma Tianyi</strong></p>
+            <p><strong>Li Tianyi</strong></p>
+            <p><strong>Zheng Shan</strong></p>
+            <p><strong>Stephan, Singh</strong></p>
+            <h2>Dream Web</h2>
+            <p>
+            The "Dream Web" website is a web forum developed by the Chinese Gang group. Its business mainly includes: </p>
+            <a href="./postlist"><p><strong>second-hand goods trading</strong></p></a>
+            <a href="./postlist"><p><strong>house rental</strong></p></a>
+            <a href="./postlist"><p><strong>second-hand car transfer</strong></p></a>
+            <a href="./postlist"><p><strong>advertising</strong></p></a>
             </div>
             <br />
             <br />
             <br />
             <div className={styles.aboutUsFooter}>
                 <div>Contact Us At</div>
-
                 <div >
-                <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" className={styles.aboutUsImg}><img src="https://pic.pngsucai.com/00/56/40/24c6b5da2b495420.jpg" width="30px" height="30px"/></a> 
-                <a href="https://www.facebook.com/tianyi.ma.10" className={styles.aboutUsImg}><img src="https://pic.pngsucai.com/00/00/52/29f5b25c3f68bbc8.jpg" width="30px" height="30px"/></a> 
-                <a href="https://wx.qq.com/" className={styles.aboutUsImg}><img src="https://mmbiz.qpic.cn/mmbiz_png/PzmdfkHXmVb72oySvPPW4l0BD2BITClxJAADYQfs19o37qb7W1ckCRTWFeURmHAundAZgQR71JvaMWP38a8WvA/640?wx_fmt=png" width="30px" height="30px"/></a> 
+                <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" className={styles.aboutUsImg}><img src="../static/gmail.png" width="30px" height="30px"/></a> 
+                <a href="https://www.facebook.com/tianyi.ma.10" className={styles.aboutUsImg}><img src="../static/facebook.png" width="30px" height="30px"/></a> 
+                <a href="https://wx.qq.com/" className={styles.aboutUsImg}><img src="../static/wechat.png" width="30px" height="30px"/></a> 
                 </div>
             </div>
         </div>
