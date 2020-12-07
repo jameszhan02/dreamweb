0ce74e3b594979e5ea075bdd19cd4d8d3abb32ec
MartinMatianyi <martin.matianyi@gmail.com>
2020-12-07 15:52:16 -0500

    navbar

--- src/modules/Navbar.js
+++ src/modules/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Navbar.css";
 import {logout, loginUser } from '../cookieHelper';
+import Avatar from "@material-ui/core/Avatar";
 
 let currentUser = loginUser();
 let isUserLogin = false;
@@ -58,7 +59,7 @@ class Navbar extends Component {
         </ul>
         <div className="iconBorder">
         {isUserLogin
-        ? <img className="personIcon" src={currentUser.data.piclink} alt="piclink" onClick={curUserDetail}/>
+        ? <Avatar  className="personIcon"  alt={currentUser.data.email} src="/broken-image.jpg" style={{marginTop: '5%'}}  onClick={curUserDetail}/>
         : <img className="personIcon" src="https://user-images.githubusercontent.com/45243472/101384466-7be14280-3888-11eb-9e05-4efe863e4072.jpg" alt="defaultPic"/>
         }
         </div>
